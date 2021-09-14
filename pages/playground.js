cc5552f73ce7706208d32e2f68484adc09bbcb73
Ashish Shukla <akshkgd@gmail.com>
2021-09-14 14:11:37 +0530

    test

---
+++ pages/playground.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div>
+       <h1 className="font-black px-20 mx-20 mt-20">Access Javascript Playground</h1>
+       <iframe src="https://ck-editor.vercel.app/" height="100%" width="100%"></iframe>
+       </div>
+    )
+  }
+  
